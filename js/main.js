5c02a0fb875a49e1c3bbbd119c366fe6092e07ee
Matias Peña <pena_matias@hotmail.com>
2023-08-09 12:15:12 -0300

    Added favicon.ico, send email from contact working correctly

--- js/main.js
+++ js/main.js
@@ -1,76 +1,68 @@
-window.onload = getAll;
+let url = window.location.href;
+if (url.includes("index.html")) {
+  window.onload = getAll;
+}
 
-function getAll(){
+function getAll() {
   fetch("https://portfolio.free.mockoapp.net/data")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((dataArray) => {
-    dataArray.forEach((data) => {
-      let projects = document.getElementById("projects");
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((dataArray) => {
+      dataArray.forEach((data) => {
+        let projects = document.getElementById("projects");
 
-      let article = document.createElement("article");
-      article.className = "projects-project";
+        let article = document.createElement("article");
+        article.className = "projects-project";
 
-      let title = document.createElement("h3");
-      title.className = "projects-project-title";
-      title.innerHTML = data.title;
+        let title = document.createElement("h3");
+        title.className = "projects-project-title";
+        title.innerHTML = data.title;
 
-      let description = document.createElement("p");
-      description.className = "projects-project-description";
-      description.innerHTML = data.description;
+        let description = document.createElement("p");
+        description.className = "projects-project-description";
+        description.innerHTML = data.description;
 
-      let url = document.createElement("a");
-      url.className = "projects-project-url";
-      url.href = data.url;
-      url.innerHTML = "Link al repositorio";
+        let url = document.createElement("a");
+        url.className = "projects-project-url";
+        url.href = data.url;
+        url.innerHTML = "Link al repositorio";
 
-      let img = document.createElement("img");
-      img.className = "projects-project-img";
-      img.src = data.image;
+        let img = document.createElement("img");
+        img.className = "projects-project-img";
+        img.src = data.image;
 
-      article.appendChild(title);
-      article.appendChild(img);
-      article.appendChild(description);
-      article.appendChild(url);
+        article.appendChild(title);
+        article.appendChild(img);
+        article.appendChild(description);
+        article.appendChild(url);
 
-      projects.appendChild(article);
-      }
-    );
-  });
-};
-
-document.addEventListener('DOMContentLoaded', function () {
-  const contactForm = document.getElementById('contact-form');
-  const resetButton = document.getElementById('reset-button');
-
-  // Initialize EmailJS with your user ID
-  emailjs.init('user_your_emailjs_user_id');
-
-  contactForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Prepare email parameters
-    const emailParams = {
-      to_email: 'pena_matias@hotmail.com',
-      from_name: contactForm.name.value,
-      from_email: contactForm.email.value,
-      message: contactForm.message.value
-    };
-
-    // Send email using EmailJS
-    emailjs.send('your_emailjs_service_id', 'your_emailjs_template_id', emailParams)
-      .then(function (response) {
-        console.log('Email sent successfully:', response);
-        // Reset the form after successful submission
-        contactForm.reset();
-      })
-      .catch(function (error) {
-        console.error('Email could not be sent:', error);
+        projects.appendChild(article);
       });
+    });
+}
+
+function sendEmail() {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
   });
 
-  resetButton.addEventListener('click', function () {
-    contactForm.reset();
-  });
-});
+  emailjs.init("ynn2rbfQvTg2GXtgv");
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  emailjs
+    .send("portfolioApp", "template_email", {
+      to_email: "pena_matias@hotmail.com",
+      from_name: name,
+      from_email: email,
+      message: message,
+    })
+    .then(function response() {
+      alert("Email sent successfully!");
+      form.reset();
+    });
+}
